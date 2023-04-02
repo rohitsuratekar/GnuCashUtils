9c5cac3d6ccb16ec7fba67d69f23c73a2ca8b11a
Rohit Suratekar <rohitsuratekar@gmail.com>
2023-04-02 15:51:20 +0530

    Rewrite Module

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from common.model import GnuBook
-from config.paths import GNU_CASH_FILE
-from visualize.expenses import run
+"""
+GnuCashUtils
+Author: Rohit Suratekar (rohitsuratekar@gmail.com)
 
+Simple utilities to handle the GNU Cash database.
+Your database should be SQLight for these scripts to work
+"""
 
-def update_zerodha():
-    book = GnuBook(GNU_CASH_FILE, readonly=False)
-    filename = "/Users/dex/Downloads/holdings.csv"
-    book.import_zerodha_holdings(filename)
-    book.save()
-
-
-if __name__ == "__main__":
-    update_zerodha()
